da247b987013066de0deb90477483cfc5412d922
Viet Le Truc <vietexob@gmail.com>
2014-12-20 23:32:03 -0500

    My world 2

--- code/myWorld.R
+++ code/myWorld.R
@@ -2,7 +2,7 @@ rm(list = ls())
 
 library(maptools)
 library(ggplot2)
-# library(rgeos)
+library(rgeos)
 
 ## Load the world map shapefile
 filename <- "./data/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp"
@@ -10,10 +10,17 @@ world <- readShapePoly(filename)
 
 ## Load my world data
 my.world <- read.csv(file = "./data/my_world2.csv", header = TRUE)
+Been_To <- world$name %in% my.world$Country2
+world@data <- data.frame(world@data, Been_To)
 
 ## Plot the world map of R downloads
-world.f <- fortify(world, region = "country")
-world.f <- merge(world.f, world@data, by.x = "id", by.y = "country")
-Map <- ggplot(world.f, aes(long, lat, group = group, fill = count)) + geom_polygon()
-(Map <- Map + ggtitle("R Activity around the World from Dec 08 to Dec 13, 2014"))
+world.f <- fortify(world, region = "name")
+world.f <- merge(world.f, world@data, by.x = "id", by.y = "name")
+Map <- ggplot(world.f, aes(long, lat, group = group, fill = Been_To)) + geom_polygon()
+(Map <- Map + ggtitle("Countries I Have Been To as of Dec 20, 2014"))
 
+## Save a really large map
+filename <- "./figures/my_world.png"
+width <- par("din")[1]
+height <- width / 1.68
+ggsave(filename, scale = 1.5, dpi = 400, width = width, height = height)
