7721f7e6d225994d8ed2ffc1cba90722ed7d062e
Truc Viet Le <vietexob@gmail.com>
2016-12-20 18:24:08 +0800

    Rerun world_pop_cartogram

--- code/cartograms/world_pop_cartogram.R
+++ code/cartograms/world_pop_cartogram.R
@@ -18,21 +18,22 @@ smaller.data <- data.frame(Country.Code = world.pop$Country.Code,
                            Population = world.pop$X2013)
 smaller.data <- na.omit(smaller.data)
 
-plot.nameStr <- "Cartogram of the World's Population by Country (2013)"
+plot.nameStr <- "Cartogram of the World Population (2013)"
 ## Join the two datasets
 matched.indices <- match(world@data[, "ISO3"], smaller.data[, "Country.Code"])
 world@data <- data.frame(world@data, smaller.data[matched.indices, ])
 
-## Have to set blur > 0 in order to work!
+## Have to set blur = 0.5 in order to work!
 world.carto <- quick.carto(world, world@data$Population, blur = 0.5)
 world.f <- fortify(world.carto, region = "Country.Code")
 world.f <- merge(world.f, world@data, by.x = "id", by.y = "Country.Code")
-Map <- ggplot(world.f, aes(long, lat, group = group, fill = world.f$Population)) +
+my_map <- ggplot(world.f, aes(long, lat, group = group, fill = world.f$Population)) +
   labs(fill = "Population") + geom_polygon() + fivethirtyeight_theme()
-(Map <- Map + ggtitle(plot.nameStr))
+(my_map <- my_map + ggtitle(plot.nameStr))
 
 ## Save a really large map
 width <- par("din")[1]
 height <- width / 1.68
 plot.filename <- "./figures/cartograms/world_pop_cartogram.png"
-ggsave(plot.filename, scale = 1.5, dpi = 400, width = width, height = height)
+ggsave(plot.filename, width = width, height = height)
+print(paste('Saved figure to file:', plot.filename))
